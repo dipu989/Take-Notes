75a7e46d3f6769fcdaf16d29171c1f1760d00f7f
Shantnu Kumar <me.shantnu@gmail.com>
2020-06-27 16:44:14 +0530

    Initializing Project

---
+++ app/src/main/java/com/example/takenotes/NewNoteActivity.java
@@ -0,0 +1,14 @@
+package com.example.takenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_note);
+    }
+}
---
+++ app/src/main/java/com/example/takenotes/TrashActivity.java
@@ -0,0 +1,14 @@
+package com.example.takenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trash);
+    }
+}
