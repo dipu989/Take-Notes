9149c05bc514f6a863dd86b7dbca033f82551565
Shantnu Kumar <me.shantnu@gmail.com>
2020-07-22 13:34:02 +0530

    WIP : Add nav drawer.
    :sparkles: Add navigation drawer layout.
    
    :sparkles: Add navigation drawer layout.

--- app/src/main/java/com/example/takenotes/Activity/MainActivity.java
+++ app/src/main/java/com/example/takenotes/Activity/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.takenotes.Activity;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,8 +19,10 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.new_note)
-    Button btnNewNote;
+    //@BindView(R.id.new_note)
+    //Button btnNewNote;
+
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +31,27 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.setDebug(true);
         ButterKnife.bind(this);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
-    @OnClick(R.id.new_note)
-    public void addNote(View view) {
-        Intent intent = new Intent(this, NewNoteActivity.class);
-        startActivity(intent);
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
+  //  @OnClick(R.id.new_note)
+   // public void addNote(View view) {
+     //   Intent intent = new Intent(this, NewNoteActivity.class);
+      //  startActivity(intent);
+   // }
 
 }
