c33416385aa3573f1867d3878862f1ee33750cd0
Shantnu Kumar <me.shantnu@gmail.com>
2020-07-22 11:14:43 +0530

    Add double tap exit functionality.

--- app/src/main/java/com/example/takenotes/Activity/MainActivity.java
+++ app/src/main/java/com/example/takenotes/Activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.takenotes.R;
 
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.new_note)
     Button btnNewNote;
+    
+    private long back_pressed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +36,18 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, NewNoteActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onBackPressed() {
+        if(back_pressed + 2000 > System.currentTimeMillis())
+            super.onBackPressed();
+        else {
+            Toast toast = Toast.makeText(getApplicationContext(), "Double tap to exit", Toast.LENGTH_SHORT);
+            toast.show();
+            back_pressed = System.currentTimeMillis();
+        }
+    }
+
+
 
 }
