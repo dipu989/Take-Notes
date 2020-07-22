0819860008ea7f7416b8dc593bdfceda461dcaf9
Shantnu Kumar <me.shantnu@gmail.com>
2020-07-22 13:34:02 +0530

    Resolved merge conflict.

--- app/src/main/java/com/example/takenotes/Activity/MainActivity.java
+++ app/src/main/java/com/example/takenotes/Activity/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.takenotes.Activity;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,11 +19,13 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.new_note)
-    Button btnNewNote;
     
     private long back_pressed;
+
+    //@BindView(R.id.new_note)
+    //Button btnNewNote;
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +35,28 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.setDebug(true);
         ButterKnife.bind(this);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
-    @OnClick(R.id.new_note)
-    public void addNote(View view) {
-        Intent intent = new Intent(this, NewNoteActivity.class);
-        startActivity(intent);
-    }
+  //  @OnClick(R.id.new_note)
+   // public void addNote(View view) {
+     //   Intent intent = new Intent(this, NewNoteActivity.class);
+      //  startActivity(intent);
+   // }
 
     @Override
     public void onBackPressed() {
-        if(back_pressed + 2000 > System.currentTimeMillis())
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else if(back_pressed + 2000 > System.currentTimeMillis())
             super.onBackPressed();
         else {
             Toast toast = Toast.makeText(getApplicationContext(), "Double tap to exit", Toast.LENGTH_SHORT);
@@ -47,7 +64,4 @@ public class MainActivity extends AppCompatActivity {
             back_pressed = System.currentTimeMillis();
         }
     }
-
-
-
 }
