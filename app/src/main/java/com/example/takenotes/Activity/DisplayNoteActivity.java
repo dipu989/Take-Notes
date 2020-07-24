ff1d13ac550d2898ccb792d71a13b6eea3cdb1ec
Shantnu Kumar <me.shantnu@gmail.com>
2020-07-24 23:37:16 +0530

    :sparkles: Display note in new activity.

--- app/src/main/java/com/example/takenotes/Activity/DisplayNoteActivity.java
+++ app/src/main/java/com/example/takenotes/Activity/DisplayNoteActivity.java
@@ -3,14 +3,45 @@ package com.example.takenotes.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.takenotes.R;
 
 public class DisplayNoteActivity extends AppCompatActivity {
 
+    EditText displayTitle;
+    EditText displayBody;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_note);
+
+        displayTitle = findViewById(R.id.display_title);
+        displayBody = findViewById(R.id.display_body);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getIntentData();
     }
+
+    private void getIntentData() {
+        if(getIntent().hasExtra("Title") && getIntent().hasExtra("Body")){
+            String title = getIntent().getStringExtra("Title");
+            String body = getIntent().getStringExtra("Body");
+            setData(title, body);
+        }
+    }
+
+    public void setData(String title, String body) {
+        displayTitle.setText(title);
+        displayBody.setText(body);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+
 }
--- app/src/main/java/com/example/takenotes/Adapter/NoteAdapter.java
+++ app/src/main/java/com/example/takenotes/Adapter/NoteAdapter.java
@@ -1,6 +1,7 @@
 package com.example.takenotes.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.takenotes.Activity.DisplayNoteActivity;
 import com.example.takenotes.Model.Note;
 import com.example.takenotes.R;
 
@@ -19,6 +21,8 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
 
     Context context;
     List<Note> noteList = new ArrayList<>();
+    private boolean multiSelect = false;
+    private List<Note> selectedNotes = new ArrayList<Note>();
 
     public NoteAdapter(Context context, List<Note> noteList) {
         this.context = context;
@@ -37,6 +41,16 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
     public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
         holder.title.setText(noteList.get(position).getNoteTitle());
         holder.body.setText(noteList.get(position).getNoteBody());
+
+        holder.body.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DisplayNoteActivity.class);
+                intent.putExtra("Title", holder.title.getText().toString());
+                intent.putExtra("Body", holder.body.getText().toString());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
